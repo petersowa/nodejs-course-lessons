b326a1ca23df7e2925db3584e518b46b5b3c0625
petersowa <petersowa@users.noreply.github.com>
2018-11-03 22:25:26 -0500

    completed users assignment

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.use('/add-product', (req, res, next) => {
   res.send('<h1>Add Product</h1>');
 });
 
+app.use('/users', (req, res, next) => {
+  const users = ['user1', 'user2', 'user3'];
+
+  res.send(
+    `<h1>Users</h1><ul>${users.reduce(
+      (acc, user) => acc + '<li>' + user + '</li>',
+      ''
+    )}</ul>`
+  );
+});
+
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello 404</h1>');
 });
