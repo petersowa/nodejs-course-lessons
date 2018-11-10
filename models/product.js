c0a792e67a77d0b181e1b0adca7926c1dbe2997f
petersowa <petersowa@users.noreply.github.com>
2018-11-09 19:40:37 -0600

    refactored product model code to use file path const

--- models/product.js
+++ models/product.js
@@ -2,21 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
 
-//const products = [];
+const productFilePath = path.join(rootDir, 'data', 'products.json');
+
 const getProductsFromFile = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(
-      path.join(rootDir, 'data', 'products.json'),
-      (err, fileContent) => {
-        let products = [];
-        if (!err) {
-          products = JSON.parse(fileContent);
-        } else {
-          console.log(err);
-        }
-        resolve(products);
+    fs.readFile(productFilePath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      } else {
+        console.log(err);
       }
-    );
+      resolve(products);
+    });
   });
 };
 
@@ -28,10 +26,8 @@ module.exports = class Product {
   async save() {
     const products = await getProductsFromFile();
     products.push(this);
-    fs.writeFile(
-      path.join(rootDir, 'data', 'products.json'),
-      JSON.stringify(products),
-      err => console.log(err ? `error: ${err}` : 'file saved')
+    fs.writeFile(productFilePath, JSON.stringify(products), err =>
+      console.log(err ? `error: ${err}` : 'file saved')
     );
   }
 
