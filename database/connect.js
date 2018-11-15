4fc29527d4c64dc45ee0f03626bf4b6ec6a69c32
Peter Sowa <devspeter@granitecode.com>
2018-11-15 13:14:43 -0600

    added connection to mongodb

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,19 @@
 const { MongoClient } = require('mongodb');
 const { db_connect } = require('./config.js');
 
+let _db;
+
 console.log('connecting to mongodb');
 
 exports.mongoConnect = () => {
-  return MongoClient.connect(
-    db_connect,
-    { useNewUrlParser: true }
+  return new Promise((resolve, reject) => {
+    if (_db) resolve(_db);
+    MongoClient.connect(
+      db_connect,
+      { useNewUrlParser: true }
+    ).then(client => {
+      _db=client;
+      resolve(_db)
+    }
   );
 };
